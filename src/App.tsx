861472ecaa581189f902fe276d9ad5a9803ff85d
Alejandro <aletomasperren@gmail.com>
2025-01-06 10:20:01 -0300

    Finish Proyect Cripto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
+import { useEffect } from "react"
+import CriptoSearchForm from "./components/CriptoSearchForm"
+import { useCryptoStore } from "./store"
+import CryptoPriceDisplay from "./components/CryptoPriceDisplay"
 
 function App() {
-  
+  const fetchCryptos = useCryptoStore((state) => state.fetchCryptos)
+
+  useEffect(() => {
+    fetchCryptos()
+  }, [])
 
   return (
     <>
-     <div className="container">
-      <h1 className="app-title">
-        Corizador de <span>Criptomonedas</span>
-      </h1>
-        <div className="content">
-          
-        </div>
-     </div>
+      <div className="container">
+          <h1 className="app-title">
+              Cotizador de <span>Criptomonedas</span>
+          </h1>
+
+          <div className="content">
+              <CriptoSearchForm />
+              <CryptoPriceDisplay />
+          </div>
+      </div>
     </>
   )
 }
